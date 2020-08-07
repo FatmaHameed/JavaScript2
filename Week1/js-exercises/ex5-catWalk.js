5b16c004cadecb01117fd69ca548967b8dc53719
Fatma Hameed <fatma78hameed@gmail.com>
2020-08-07 10:28:58 -0700

    finished exercise5 js2w1 added the function catWalk to move the cat

--- Week1/js-exercises/ex5-catWalk.js
+++ Week1/js-exercises/ex5-catWalk.js
@@ -1,40 +1,59 @@
-/**
- 
- ** Exercise 5: The cat walk **
- Starting with an HTML, which has a single img tag of an animated GIF of a cat walking.
+const origCat = document.querySelector('img');
+origCat.style.left = '0px';
+const bodyWidth = window.screen.width;
+const dancingCat = document.createElement('img');
 
- 1. Create a variable to store a reference to the img.
- 2. Change the style of the img to have a "left" of "0px", so that it starts at the left hand of the screens.
- 3. Create a  function called catWalk() that moves the cat 10 pixels to the right of where it started, by changing the "left" style property.
- 4. Call that function every 50 milliseconds.Your cat should now be moving across the screen from left to right.Hurrah!
- 5. When the cat reaches the right - hand of the screen, restart them at the left hand side("0px").So they should keep walking from left to right across the screen, forever and ever.
- 6. When the cat reaches the middle of the screen, replace the img with an image of a cat dancing(use this URL: https: //tenor.com/StFI.gif), keep it dancing for 5 seconds, and then replace the img with the original image and have it continue the walk.
- 
-*/
-
-//1. Create a variable to store a reference to the img.
-const catImg = document.querySelector('img');
-// 2. Change the style of the img to have a "left" of "0px", so that it starts at the left hand of the screens.
-catImg.style.left = '0px';
-let left = catImg.style.left;
-
-//3. Create a  function called catWalk() that moves the cat 10 pixels to the right of where it started, by changing the "left" style property.
+dancingCat.src =
+  'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif?itemid=10561424';
+document.body.appendChild(dancingCat);
+dancingCat.style.position = 'absolute';
+// dancingCat.style.marginLeft = 'auto';
+// dancingCat.style.marginRight = 'auto';
+// dancingCat.style.left = '0px';
+// dancingCat.style.right = '0px';
+// dancingCat.style.textAlign = 'center';
+dancingCat.style.display = 'none';
 function catWalk() {
   let post = 0;
-  let time = 1;
-  let bodyWidth = document.body.style.maxWidth;
-  bodyWidth = 950;
-  let move = setInterval(frame, time);
 
-  function frame() {
-    if (post === bodyWidth) {
-      clearInterval(move);
+  const move1 = setInterval(firstMove, 10);
+  const move2 = setInterval(moveAgain, 24);
+  function firstMove() {
+    if (post === bodyWidth / 2) {
+      clearInterval(move1);
+      dancingCat.style.display = 'block';
+      origCat.style.display = 'none';
     } else {
       post++;
-      catImg.style.left = post + 'px';
+      origCat.style.left = post + 'px';
+      dancingCat.style.left = post + 'px';
     }
   }
+  function moveAgain() {
+    if (post === bodyWidth / 2) {
+      post++;
+      origCat.style.left = post + 'px';
+      showCode();
+    }
+    if (post === bodyWidth) {
+      clearInterval(move2);
+    } else {
+      post++;
+      origCat.style.left = post + 'px';
+    }
+  }
+  function showLoad() {
+    dancingCat.style.display = 'block';
+    origCat.style.display = 'none';
+  }
+
+  function showCode() {
+    origCat.style.display = 'block';
+    dancingCat.style.display = 'none';
+  }
+  setTimeout(showLoad, 5000);
+  setTimeout(showCode, 10000);
 }
 
 setTimeout(catWalk, 50);
-setInterval(catWalk, 5000);
+setInterval(catWalk, 20000);
