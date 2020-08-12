ddae0b2ec96b5ca9f6873634572d54f18d04590e
Fatma Hameed <fatma78hameed@gmail.com>
2020-08-12 06:39:35 -0700

    finished ex5 w2 js2- the favorite hobbies

--- Week2/js-exercises/ex5-myFavoriteHobbies.js
+++ Week2/js-exercises/ex5-myFavoriteHobbies.js
@@ -10,6 +10,22 @@
 
 function createHTMLList(arr) {
   // your code goes in here
+
+  const container = document.getElementById('container');
+  container.style.margin = '30px';
+  document.querySelector('h1').style.color = 'darkBlue';
+  document.querySelector('h1').style.fontSize = '3rem';
+  // console.log(container);
+
+  const unOrderedList = document.createElement('ul');
+  const hobbiesList = arr.map(item => {
+    const listHobby = document.createElement('li');
+    listHobby.innerText = item;
+    listHobby.style.fontSize = '2rem';
+    unOrderedList.appendChild(listHobby);
+  });
+  container.appendChild(unOrderedList);
+  return hobbiesList;
 }
 
 const myHobbies = [
@@ -19,3 +35,4 @@ const myHobbies = [
   'Hanging out with friends',
   'Going to the gym',
 ];
+createHTMLList(myHobbies);
