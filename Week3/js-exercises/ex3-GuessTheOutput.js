86e6cd5d41014a28feb4edc5d8c208949122df21
Fatma Hameed <fatma78hameed@gmail.com>
2020-08-19 12:18:18 -0700

    finished ex3 w3 js2 (guess the output)

--- Week3/js-exercises/ex3-GuessTheOutput.js
+++ Week3/js-exercises/ex3-GuessTheOutput.js
@@ -8,14 +8,12 @@ Write out your reasoning in 50 words or less.
 
 */
 
-
-
 let a = 10;
-const x = (function () {
-  a = 12;
-  return function () {
+const x = (function() {
+  a = 12; // re-assigned the same variable to another value, so if the syntax is correct, the output here must be alerting the new value of a, which is 12
+  return function() {
     alert(a);
   };
-})();
+})(); // here I guess that the output will be syntax error since these is un-necessary () in the end
 
-x();
+x(); // The output here supposed to be 12, because the global variable is assigned to another value inside the function. The code will be read as it is in the block scope, because it is read from bottom to up.
