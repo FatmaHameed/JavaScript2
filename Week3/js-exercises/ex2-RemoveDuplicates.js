fa42c5d5b8944fbc68d0e52e3276dca6e537db89
Fatma Hameed <fatma78hameed@gmail.com>
2020-08-19 06:26:28 -0700

    finished exercise2 js2 w3 (Remove Duplicates)

--- Week3/js-exercises/ex2-RemoveDuplicates.js
+++ Week3/js-exercises/ex2-RemoveDuplicates.js
@@ -10,12 +10,20 @@ does not return anything but removes any duplicate elements from the array.
    
  */
 
-
 // WRITE YOUR FUNCTION HERE
 
 const letters = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c', 'b'];
+function removeDuplicates(arr) {
+  const filterDuplicates = arr.filter(function(letter, letterPos) {
+    // filter the array and return array if the current position of the letter (after looping) is equal to the previous position (in the original array)
+    return arr.indexOf(letter) === letterPos; // the current position of the letter will not be equal to the position in the original array for the duplicates.
+  });
 
-removeDuplicates(letter);
+  return filterDuplicates;
+}
+removeDuplicates(letters);
 
-if (letters === ['a', 'b', 'c', 'd', 'e', 'f'])
-  console.log("Hooray!")
+if ((letters === ['a', 'b', 'c', 'd', 'e', 'f'], [])) {
+  // as explained, arrays are objects, so they are not equals. That is why we need to compare between them as strings, so we add []
+  console.log('Hooray!');
+}
