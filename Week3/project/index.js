1a748054473ed386e8e10d8e84be49130a974b16
Fatma Hameed <fatma78hameed@gmail.com>
2020-08-22 09:12:13 -0700

    modified the comments and deleted unnecessary comments

--- Week3/project/index.js
+++ Week3/project/index.js
@@ -10,25 +10,20 @@ const calculate = document.getElementById('calculate');
 const tipAmount = document.getElementById('tip');
 const amountE = document.getElementById('amountE');
 const eachE = document.getElementById('eachE');
-// 2- add calculation functions: the calculate function will loop through the options and add conditionality according to the option selected
+// 2- add calculation functions: the calculate function will create the calculations.
 
 function calculateTipAmount() {
-  // create validate function to alert if there is a field missing
-  // if (billAmount.value > 0 && optionsA.value > 0 && peopleNum.value > 0) {
-  //   const totalTip = (billAmount.value * optionsA.value) / peopleNum.value;
-  //   const tip = totalTip.toFixed(2);
-  //   tipAmount.innerText = tip;
-  // }
+  // create the calculations of the tip
   const totalTip = (billAmount.value * optionsA.value) / peopleNum.value;
   const tip = totalTip.toFixed(2);
   tipAmount.innerText = tip;
-
+  // create validate function to alert if there is a field missing
   if (billAmount.value == 0 || optionsA.value == 0 || peopleNum.value == '') {
     alert('You have to fill all fields');
     amountE.style.display = 'none';
     eachE.style.display = 'none';
   }
-
+  // create conditionality of the function and display the amount of tip
   if (peopleNum.value > 1 && billAmount.value > 0 && optionsA.value > 0) {
     amountE.style.display = 'block';
     eachE.style.display = 'block';
@@ -42,8 +37,5 @@ function calculateTipAmount() {
   }
 }
 
-// 3- add event listeners for the button
+// 3- add event listeners for the calculate
 calculate.addEventListener('click', calculateTipAmount);
-// 4- display the amount
-
-// 5- create a function to alert if there is a field missing
