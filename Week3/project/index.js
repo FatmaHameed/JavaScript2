09fbd17b41ae6cfb8f35cb9adb7f2de916b5653b
Fatma Hameed <fatma78hameed@gmail.com>
2020-08-22 08:40:35 -0700

    fixed the functionality of the app.

--- Week3/project/index.js
+++ Week3/project/index.js
@@ -3,36 +3,47 @@
 // document.querySelector("#app").innerText = "Tip Calculator";
 
 // 1-  grasp the dom elements that we need to add the functionality:amountOfBill input, form-control: to loop through it and put the functionality and calculations, peopleNum input to be targeted with the calculates, button, final p elements
-const billAmount = parseFloat(document.getElementById('amountOfBill').value);
-const options = parseFloat(document.getElementsByClassName('form-control').value);
-const peopleNum = parseInt(document.getElementById('peopleNum').value, 10);
-const button = document.querySelector('button');
+const billAmount = document.getElementById('amountOfBill');
+const optionsA = document.getElementById('form-control');
+const peopleNum = document.getElementById('peopleNum');
+const calculate = document.getElementById('calculate');
 const tipAmount = document.getElementById('tip');
-const amount = document.getElementById('amount');
-
+const amountE = document.getElementById('amountE');
+const eachE = document.getElementById('eachE');
 // 2- add calculation functions: the calculate function will loop through the options and add conditionality according to the option selected
 
 function calculateTipAmount() {
   // create validate function to alert if there is a field missing
-  if (billAmount === 0 || options === 0 || peopleNum === '') {
-    const alert = 'You have to fill all fields';
-    alert();
+  // if (billAmount.value > 0 && optionsA.value > 0 && peopleNum.value > 0) {
+  //   const totalTip = (billAmount.value * optionsA.value) / peopleNum.value;
+  //   const tip = totalTip.toFixed(2);
+  //   tipAmount.innerText = tip;
+  // }
+  const totalTip = (billAmount.value * optionsA.value) / peopleNum.value;
+  const tip = totalTip.toFixed(2);
+  tipAmount.innerText = tip;
+
+  if (billAmount.value == 0 || optionsA.value == 0 || peopleNum.value == '') {
+    alert('You have to fill all fields');
+    amountE.style.display = 'none';
+    eachE.style.display = 'none';
   }
 
-  const totalTip = billAmount * options * peopleNum;
-  tipAmount.innerText = totalTip;
-
-  if (peopleNum > 1) {
-    amount.style.display = 'block';
-    document.getElementById('each').style.display = 'block';
-    tipAmount.innerText = totalTip;
-  } else {
-    document.getElementById('each').style.display = 'none';
+  if (peopleNum.value > 1 && billAmount.value > 0 && optionsA.value > 0) {
+    amountE.style.display = 'block';
+    eachE.style.display = 'block';
+  } else if (peopleNum.value == 1 && billAmount.value > 0 && optionsA.value > 0) {
+    amountE.style.display = 'block';
+    eachE.style.display = 'none';
+  } else if (peopleNum.value <= 0 || billAmount.value <= 0) {
+    amountE.style.display = 'none';
+    eachE.style.display = 'none';
+    alert('Minus or zero amount is not accepted');
   }
 }
 
 // 3- add event listeners for the button
-button.addEventListener('click', calculateTipAmount);
+calculate.addEventListener('click', calculateTipAmount);
 // 4- display the amount
 
 // 5- create a function to alert if there is a field missing
